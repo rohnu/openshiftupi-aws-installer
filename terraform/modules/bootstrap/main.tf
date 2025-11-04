828884b71977d77b232fbb3b3a016f95891b0b81
Ramprasad Ohnu <rohnu@extrao1.com>
2025-11-04 12:48:51 -0600

    Update main.tf
    Signed-off-by: Ramprasad Ohnu <rohnu@extrao1.com>

--- terraform/modules/bootstrap/main.tf
+++ terraform/modules/bootstrap/main.tf
@@ -8,19 +8,25 @@ variable "ignition_b64" { type = string }
 variable "name_prefix" { type = string }
 variable "tags" { type = map(string) }
 
-resource "aws_instance" "this" {
-  ami                         = var.ami_id
-  instance_type               = var.instance_type
-  subnet_id                   = var.subnet_id
-  vpc_security_group_ids      = var.sg_ids
-  iam_instance_profile        = var.iam_instance_profile
-  key_name                    = var.key_name
-  user_data_base64            = var.ignition_b64
-  monitoring                  = true
-  disable_api_termination     = false
-
-  tags = merge(var.tags, { Name = var.name_prefix })
+resource "aws_instance" "bootstrap" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+  key_name               = var.key_name
+  vpc_security_group_ids = var.sg_ids
+  iam_instance_profile   = var.iam_instance_profile
+  
+  user_data_base64 = var.ignition_b64
+  
+  tags = merge(var.tags, {
+    Name = var.name_prefix
+  })
 }
 
-output "instance_id" { value = aws_instance.this.id }
-output "private_ip"  { value = aws_instance.this.private_ip }
+output "private_ip" {
+  value = aws_instance.bootstrap.private_ip
+}
+
+output "instance_id" {
+  value = aws_instance.bootstrap.id
+}
